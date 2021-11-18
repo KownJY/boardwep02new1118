0a12e8124b1bacaf8ba5e1c356c0c648d8a3a7b1
name <eungi960@gmail.com>
2021-11-19 02:09:22 +0900

    add table

--- src/main/java/com/koreait/board2/model/UserVO.java
+++ src/main/java/com/koreait/board2/model/UserVO.java
@@ -56,3 +56,29 @@ public class UserVO {
         this.rdt = rdt;
     }
 }
+//    DROP TABLE t_user;
+//        DROP TABLE t_board;
+//
+//        CREATE TABLE t_user(
+//
+//        iuser INT unsigned PRIMARY KEY AUTO_INCREMENT,
+//        uid VARCHAR(20) unique NOT NULL,
+//        upw VARCHAR(50) NOT NULL,
+//        nm VARCHAR(10) NOT NULL,
+//        gender INT unsigned CHECK(gender IN(0,1)),
+//        rdt datetime DEFAULT NOW()
+//
+//        );
+//
+//
+//        CREATE TABLE t_board(
+//
+//        iboard INT PRIMARY KEY AUTO_INCREMENT,
+//        tile VARCHAR(100) NOT NULL,
+//        ctnt VARCHAR(2000) NOT NULL,
+//        writer INT UNSIGNED NOT NULL,
+//        FOREIGN KEY (writer) REFERENCES t_user(iuser),
+//        rdt DATETIME DEFAULT NOW(),
+//        mdt DATETIME DEFAULT NOW()
+//
+//        );
